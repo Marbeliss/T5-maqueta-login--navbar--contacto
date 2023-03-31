05c7ec392c6ebefb8ca37dd4d08de72046398fb6
Marbelis Poleo <poleomoncada@gmail.com>
2023-03-31 03:24:56 -0300

    Nombre de la carpeta o comentario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
